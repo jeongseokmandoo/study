4c2f071b45864ea641bd231d6883896e6a6f88ae
jeongseokmandoo <127060327+jeongseokmandoo@users.noreply.github.com>
2023-07-31 18:57:06 +0900

    백준 1504

--- algorithm/study/3rd/bj_1504.py
+++ algorithm/study/3rd/bj_1504.py
@@ -6,15 +6,16 @@ INF = int(1e9)
 
 n, e = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
-distance = [INF] * (n + 1)
 
 for _ in range(e):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
+    graph[b].append((a, c))
 
 v1, v2 = map(int, input().split())
 
 def dijkstra(start):
+    distance = [INF] * (n + 1)
     q = []
     heappush(q, (0, start))
     distance[start] = 0
@@ -27,18 +28,20 @@ def dijkstra(start):
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heappush(q, (cost, i[0]))
+    return distance
 
 result = []
 
-dijkstra(1)
-first_v1 = distance[v1]
-first_v2 = distance[v2]
-dijkstra(v1)
-second_v2 = distance[v2]
-v1_n = distance[n]
-dijkstra(v2)
-second_v1 = distance[v1]
-v2_n = distance[n]
+
+distance_1 = dijkstra(1)
+first_v1 = distance_1[v1]
+first_v2 = distance_1[v2]
+distance_v1 = dijkstra(v1)
+second_v2 = distance_v1[v2]
+v1_n = distance_v1[n]
+distance_v2 = dijkstra(v2)
+second_v1 = distance_v2[v1]
+v2_n = distance_v2[n]
 
 result.append(first_v1 + second_v2 + v2_n)
 result.append(first_v2 + second_v1 + v1_n)
