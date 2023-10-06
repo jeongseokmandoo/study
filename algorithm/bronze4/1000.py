b5d962d77f3797a6c46a995d4f173e9665ecd7a1
jeongseokmandoo <127060327+jeongseokmandoo@users.noreply.github.com>
2023-10-07 00:18:36 +0900

    bj bronze4

---
+++ algorithm/bronze4/1000.py
@@ -0,0 +1,17 @@
+a, b = map(int, input().split())
+
+result = a + b
+
+print(result)
+
+a, b = map(int, input().split())
+
+result = a - b
+
+print(result)
+
+a, b = map(int, input().split())
+
+result = a / b
+
+print(result)
