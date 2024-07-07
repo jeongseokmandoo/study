6f8ef70b04d5fb0f8ef713273bce8a2e000cc8ab
정석민 <jyeong1212@naver.com>
2024-07-07 22:00:37 +0900

    2024.7.6

--- algorithm/class/class2/1654.py
+++ algorithm/class/class2/1654.py
@@ -1,27 +1,54 @@
+# import sys
+# input = sys.stdin.readline
+
+# k, n = map(int, input().split())
+# data_list = []
+# length = 0
+# for _ in range(k):
+#     data = int(input().strip())
+#     data_list.append(data)
+#     length += data
+
+# value = int(length // n)
+# if value <= 2**31 - 1:
+#     pass
+# else:
+#     value = 2**31 - 1
+
+# count = 0
+# while value > 0:
+#     for data in data_list:
+#         count += data // value
+#     if count >= n:
+#         print(value)
+#         break
+#     value -= 1   
+#     count = 0     
+
 import sys
 input = sys.stdin.readline
 
 k, n = map(int, input().split())
 data_list = []
-length = 0
 for _ in range(k):
     data = int(input().strip())
     data_list.append(data)
-    length += data
 
-value = int(length // n)
-if value <= 2**31 - 1:
-    pass
-else:
-    value = 2**31 - 1
-
-count = 0
-while value > 0:
+def count_pieces(length):
+    count = 0
     for data in data_list:
-        count += data // value
-    if count >= n:
-        print(value)
-        break
-    value -= 1   
-    count = 0     
+        count += data // length
+    return count
 
+start, end = 1, max(data_list)
+answer = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    if count_pieces(mid) >= n:
+        answer = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(answer)
